db5a3e66e001d2b8b4e7d5bee09950e9d5de75c8
Edielson Assis <edielsonnice@hotmail.com>
2023-09-18 08:59:16 -0300

    Project created

---
+++ src/main/java/com/edielson/forum/forumalura/ForumAluraApplication.java
@@ -0,0 +1,12 @@
+package com.edielson.forum.forumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumAluraApplication.class, args);
+	}
+}
